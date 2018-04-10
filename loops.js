62504a350aa7c4883ba1a12c9d6c33542c68b8f0
Susanna Kosonen <kosonen.susanna@gmail.com>
2018-04-10 14:20:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function doWhileLoop(array) {
     return Math.random() >= 0.5;
   }
   do {
-    array.pop();
+    array.length--;
   } while (array.length > 0 && maybeTrue());
   return array;
 }
